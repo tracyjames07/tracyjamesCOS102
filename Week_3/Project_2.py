c49dffc2bfff7430f80b06905a31a2c73c68f65e
tracyjamesCOS101 <theresaoweziejames@outlook.com>
2024-04-02 13:15:03 +0100

    (My second commit.)

--- Week_3/Project_2.py
+++ Week_3/Project_2.py
@@ -5,13 +5,11 @@ print("a. The Roots of a Quadratic Equation Using the Quadratic Formula.")
 print("b. The Roots of a Cubic Equation Using Cardano's Formula.")
 print("Press 'A' for the 'Quadratic Function', and 'B' for the 'Cubic Function'.")
 
-quadratic_function = "A"
-cubic_function = "B"
-
 function = input("Enter an option: ")
 
 if function == "A":
-    print("We'll be solving for the roots of quadratic equation.")
+
+    print("We'll be solving for the roots of a quadratic equation.")
     print("The formula is [x = (-b ± √(b² - 4ac)) / (2a)].")
     print("You just need to enter values for 'a', 'b', and 'c'.")
     print("Please note that they must be decimal values.")
@@ -20,4 +18,11 @@ if function == "A":
     b = float(input("Enter a value for 'b': "))
     c = float(input("Enter a value for 'c': "))
 
-    quadratic_root_a = ((-b + (((b) ** 2.0) - (4 * a * c)) ** (1/2))) / (2 * a))
+    quadratic_root_a = (-b + (((b ** 2.0) - (4 * a * c)) ** (1/2))) / (2 * a)
+    quadratic_root_b = (-b - (((b ** 2.0) - (4 * a * c)) ** (1/2))) / (2 * a)
+
+    print("The roots of the given quadratic equation are " + str(quadratic_root_a) + " and " + str(quadratic_root_b) + ".")
+
+elif function == "B":
+
+    print("We'll be solving for the roots of a cubic equation using Cardano's formula.")
