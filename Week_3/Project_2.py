87bb06ec7d407f4895b10bd8307247927ca305fe
tracyjamesCOS101 <theresaoweziejames@outlook.com>
2024-04-02 16:13:08 +0100

    (My second commit.)

--- Week_3/Project_2.py
+++ Week_3/Project_2.py
@@ -1,13 +1,14 @@
 import math
 
 print("Hello. Which program would you like to perform?")
-print("a. The Roots of a Quadratic Equation Using the Quadratic Formula.")
-print("b. The Roots of a Cubic Equation Using Cardano's Formula.")
-print("Press 'A' for the 'Quadratic Function', and 'B' for the 'Cubic Function'.")
+print("1. The Roots of a Quadratic Equation Using the Quadratic Formula.")
+print("2. The Roots of a Cubic Equation Using Cardano's Formula.")
+print("Press '1' for the 'Quadratic Function', and '2' for the 'Cubic Function'.")
 
-function = input("Enter an option: ")
+function = int(input("Enter an option: "))
+print(function)
 
-if function == "A":
+if function == 1:
 
     print("We'll be solving for the roots of a quadratic equation.")
     print("The formula is [x = (-b ± √(b² - 4ac)) / (2a)].")
@@ -21,8 +22,8 @@ if function == "A":
     quadratic_root_a = (-b + (((b ** 2.0) - (4 * a * c)) ** (1/2))) / (2 * a)
     quadratic_root_b = (-b - (((b ** 2.0) - (4 * a * c)) ** (1/2))) / (2 * a)
 
-    print("The roots of the given quadratic equation are " + str(quadratic_root_a) + " and " + str(quadratic_root_b) + ".")
+    print(f"The roots of the given quadratic equation are {quadratic_root_a} and {quadratic_root_b}.")
 
-elif function == "B":
+elif function == 2:
 
     print("We'll be solving for the roots of a cubic equation using Cardano's formula.")
